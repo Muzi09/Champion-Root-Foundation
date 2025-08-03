02170aabc2c3146bcb9b7c3a39053830436bea5e
Muzi08 <thewebvana@gmail.com>
2025-08-03 23:44:55 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,31 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import Image from "next/image";
+
+import { ChartAreaInteractive } from "@/components/chart-area-interactive"
+import { DataTable } from "@/components/data-table"
+import { SectionCards } from "@/components/section-cards"
+import { SiteHeader } from "@/components/site-header"
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar"
+
+
+
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } 
+      }
+    >
+      <AppSidebar variant="inset" />
+    </SidebarProvider>
+    </div>
+  );
+}
---
+++ src/components/app-sidebar.jsx
@@ -0,0 +1,98 @@
+"use client"
+
+import * as React from "react"
+import {
+  IconBrandAppgallery,
+  IconBrandTeams,
+  IconCalendar,
+  IconCamera,
+  IconChartBar,
+  IconDashboard,
+  IconDatabase,
+  IconFileAi,
+  IconFileDescription,
+  IconFileWord,
+  IconFolder,
+  IconHelp,
+  IconHome,
+  IconInfoHexagon,
+  IconInnerShadowTop,
+  IconListDetails,
+  IconReport,
+  IconSearch,
+  IconSettings,
+  IconUsers,
+} from "@tabler/icons-react"
+
+import { NavDocuments } from "@/components/nav-documents"
+import { NavMain } from "@/components/nav-main"
+import { NavSecondary } from "@/components/nav-secondary"
+import { NavUser } from "@/components/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Home",
+      url: "#",
+      icon: IconHome,
+    },
+    {
+      title: "Our Teams",
+      url: "#",
+      icon: IconUsers,
+    },
+    {
+      title: "Match Fixtures & Results",
+      url: "#",
+      icon: IconCalendar,
+    },
+    {
+      title: "Gallery",
+      url: "#",
+      icon: IconBrandAppgallery,
+    },
+    {
+      title: "About Us",
+      url: "#",
+      icon: IconInfoHexagon,
+    },
+  ]
+}
+
+export function AppSidebar({
+  ...props
+}) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild className="data-[slot=sidebar-menu-button]:!p-1.5">
+              <a href="#">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">Champion Root Foundation</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+    </Sidebar>
+  );
+}
