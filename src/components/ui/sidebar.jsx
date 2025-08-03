0b50cf33f09c7951fae96d8cf2ae783b42fb799b
Muzi08 <muzammilkhan.code@gmail.com>
2025-08-04 00:16:42 +0530

    fix sidebar

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -156,6 +156,7 @@ function Sidebar({
   if (isMobile) {
     return (
       <Sheet open={openMobile} onOpenChange={setOpenMobile} {...props}>
+        <SidebarTrigger className="absolute top-2 left-2 z-50" />
         <SheetContent
           data-sidebar="sidebar"
           data-slot="sidebar"
@@ -538,7 +539,7 @@ function SidebarMenuAction({
         "peer-data-[size=lg]/menu-button:top-2.5",
         "group-data-[collapsible=icon]:hidden",
         showOnHover &&
-          "peer-data-[active=true]/menu-button:text-sidebar-accent-foreground group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 md:opacity-0",
+        "peer-data-[active=true]/menu-button:text-sidebar-accent-foreground group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 md:opacity-0",
         className
       )}
       {...props} />
